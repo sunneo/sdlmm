6fd9aecdcd3dda2e55e3dc713cd63940d70409b3
sunneo <sunneo529@hotmail.com>
2015-08-02 22:30:16 +0800

    add fillxy

--- sdlmm.h
+++ sdlmm.h
@@ -6,7 +6,6 @@
 void screen(int width,int height);
 void screentitle(const char* title);
 void setalwaysflush(int doflush);
-void setusealpha(int usealpha);
 void flushscreen();
 
 
@@ -16,13 +15,14 @@ void drawcircle(int x,int y,double r,int color) ;
 void drawpixel(int x,int y,int color);
 void drawline(int x1,int y1,int x2,int y2,int color);
 void drawrect(int x,int y,int w,int h,int color);
+void fillxy(int x,int y,int color);
 void fillrect(int x,int y,int w,int h,int color);
 void fillcircle(int x,int y,double r,int color);
 void drawpixels(int* pixels,int x,int y,int w,int h);
-void drawpixels2(int* pixels,int x,int y,int w,int h,int transkey);
+void drawpixels2(int* pixels,int x,int y,int w,int h,int colorkey);
+
 //load picture
-void loadimage(const char* filename,int** ret,int* w,int *h); 
-void copyscreen(int** ret,int x,int y,int w,int h);
+void loadimage(const char* filename,int** ret,int* w,int *h);
 ///////////////////////////////////////////////////////////
 
 
--- test.c
+++ test.c
@@ -1,43 +1,61 @@
 #include "sdlmm.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
-static char keymsg_buf[1024];
-static int hz=0;
-static int valmap[3]={0,1,-1};
-static const char* keystr[3]={"None","Up","Down"};
-static int keystate;
-
-static void onkeyPress(){
-    hz += valmap[keystate];
-    sprintf(keymsg_buf,"%-4s:%d",keystr[keystate],hz);
+#include <sched.h>
+#define MAXSIZE 10000
+static int prevClickX = 0;
+static int prevClickY = 0;
+static int circnt;
+struct Circle {int x,y,r,c,active;};
+struct Circle circles[MAXSIZE];
+static void touchfnc(int x,int y,int on) {
+    prevClickX = x;
+    prevClickY = y;
+    if(on) {
+        int select = circnt%MAXSIZE;
+        circles[select].x = x;
+        circles[select].y = y;
+        circles[select].r = 1;
+        circles[select].active = 1;
+        circles[select].c = rand();
+        ++circnt;
+        if(circnt >= MAXSIZE) circnt = 0;
+    }
 }
-
-static void keyfnc(int key,int ctrl,int on){
-    if(!on){
-        keystate = 0;
-        return;
-    }
-    switch(key){
-        default: return;
-        case 273: keystate = 1; break;
-        case 274: keystate = 2; break;
-        case 285: if(ctrl == 256) exit(0);
-    }
+static void mousefnc(int x,int y,int on,int btn) {
+    touchfnc(x,y,on);
 }
 int main(int argc, char *argv[]) {
-    static const int width =250;
-    static const int height = 100;
+    static const int width =1024;
+    static const int height = 768;
+    static const int max_radius = 300;
+    int i;
     screen(width,height);
-    screentitle("Press Up/Down Key");
-    settextfont("FreeMono.ttf",20);
-    setonkey(keyfnc);
+    screentitle("Draw Rectangles,Use Circle Param On Click");
+    setonmotion(touchfnc);
+    setonmouse(mousefnc);
     while(1) {
         fillrect(0,0,width,height,0xffffff);
-        drawtext( "Press Up/Down Key",0 ,0,0);
-        onkeyPress();
-        drawtext(keymsg_buf ,0,45,0);        
+        for(i=0; i<circnt; ++i) {
+           if(circles[i].active) {
+               drawline(circles[i].x-circles[i].r,circles[i].y+circles[i].r,circles[i].x,circles[i].y-circles[i].r,circles[i].c);
+               drawline(circles[i].x,circles[i].y-circles[i].r,circles[i].x+circles[i].r,circles[i].y+circles[i].r,circles[i].c);
+               drawline(circles[i].x-circles[i].r,circles[i].y+circles[i].r,circles[i].x+circles[i].r,circles[i].y+circles[i].r,circles[i].c);
+               fillxy(circles[i].x,circles[i].y,circles[i].c);
+
+               //drawrect(circles[i].x-circles[i].r,circles[i].y-circles[i].r,circles[i].r*2,circles[i].r*2,circles[i].c);
+               circles[i].r+=1;
+               if(circles[i].r>=max_radius)
+                   circles[i].active = 0;
+           }
+           else{
+              sched_yield();
+           }
+        }
         flushscreen();
+        delay(1);
     }
     return 0;
 }
